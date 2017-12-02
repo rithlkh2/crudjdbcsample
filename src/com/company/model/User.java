5e658cf0fbb3d738faf68aace6a1271afaba801e
r.ron <r.ron@gl-f.com>
2017-12-02 17:00:40 +0700

    add run

---
+++ src/com/company/model/User.java
@@ -0,0 +1,64 @@
+package com.company.model;
+
+/*
+Create By: Ron Rith
+Create Date: 12/2/2017
+*/
+public class User {
+    private Integer id;
+    private String username;
+    private String password;
+    private String fullname;
+    private String email;
+
+    public User(Integer id, String username, String password, String fullname, String email) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.fullname = fullname;
+        this.email = email;
+    }
+
+    public User() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFullname() {
+        return fullname;
+    }
+
+    public void setFullname(String fullname) {
+        this.fullname = fullname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
